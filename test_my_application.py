44e930b1cd2cb94aba3fc0b9085cda72dc0275bb
zanedotmp4 <77503127+zanedotmp4@users.noreply.github.com>
2023-06-16 11:59:48 -0400

    added screenshots

--- test_my_application.py
+++ test_my_application.py
@@ -1,3 +1,4 @@
+import random
 import re
 
 import pytest
@@ -18,6 +19,7 @@ def run(playwright: Playwright) -> None:
     expect(page).to_have_url(re.compile(".*hse/incident-mgt"))
     page.locator("div").filter(has_text=re.compile(r"^Create Incident$")).nth(1).click()
     page.get_by_label("Clear").check()
+    '''
     person=page.get_by_text("Ian Ashman")
     person.click()
     person.page.mouse.wheel(float(0),float(1))
@@ -38,20 +40,11 @@ def run(playwright: Playwright) -> None:
             pickedPerson.click(timeout=100)
         except:
             person.page.mouse.wheel(float(0),float(1))
-    
+    '''
     page.get_by_label("Incident Reported").click()
-    page.get_by_role("cell", name="15").get_by_text("15").click()
-    page.get_by_text("16").nth(1).click()
-    page.get_by_text("47").click()
-    page.get_by_role("button", name="OK").click()
-
-    page.get_by_label("Date & Time of Incident").click()
-    page.get_by_role("cell", name="15").get_by_text("15").click()
-    page.locator("li:nth-child(19) > .ant-picker-time-panel-cell-inner").first.click()
-    page.get_by_role("listitem").filter(has_text="46").locator("div").click()
-    page.get_by_role("button", name="OK").click()
-
-    page.get_by_label("Occupational Illness/Personal Injury").check()
+    hours = page.get_by_role("list").filter(has_text=re.compile(r"^000102030405060708091011121314151617181920212223$"))
+    
+    '''page.get_by_label("Occupational Illness/Personal Injury").check()
     page.locator("[id=\"__next\"] div").filter(has_text="CreateThis form is to be completed and submitted to the HSSE, Insurance & HR Dep").nth(2).click()
     page.get_by_placeholder("Enter please provide a brief description of the incident (describe damage to plant and equipment)...").fill("This is a test ")
     page.get_by_placeholder("Enter was the person(s) on a site visit or performing works in accordance with a sop, jsa or permit to work at the time of the incident?...").click()
@@ -90,7 +83,7 @@ def run(playwright: Playwright) -> None:
     page.get_by_placeholder("Enter medical diagnosis (where applicable) ...").fill("This is a medical diagnosis")
     page.get_by_placeholder("Enter comments...").click()
     page.get_by_placeholder("Enter comments...").fill("This is a comment")
-    page.get_by_role("button", name="plus Create").click()
+    page.get_by_role("button", name="plus Create").click()'''
 
 with sync_playwright() as playwright:
     run(playwright)
